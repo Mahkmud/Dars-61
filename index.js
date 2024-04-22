49895b0310b03d584607309615ce0c8a21411706
Mahmudjon Shukurov <sukurovmahmudzon15@gmail.com>
2024-04-22 09:09:33 +0300

    tugadi

---
+++ index.js
@@ -0,0 +1,29 @@
+let word = prompt("Havta kunini kiriting");
+
+if (word === "monday") {
+    alert("dushanba");
+}
+
+if (word === "tuesday") {
+    alert("seshanba");
+}
+
+if (word === "wedn") {
+alert("chorshanba")
+}
+
+if (word === "thrusday") {
+    alert("payshanba");
+}
+
+if (word === "friday") {
+    alert("juma");
+}
+
+if (word === "saturday") {
+    alert("shanba");
+}
+
+if (word === "sunday") {
+    alert("yakshanba");
+}
